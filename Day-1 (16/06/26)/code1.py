2efcea6c6ad1fc6a52f3b5afbf78c1ade2f55d10
[Soham Srivastava] <sohamsri17@gmail.com>
2026-06-16 13:36:46 +0530

    Initial Commit

---
+++ Day-1 (16/06/26)/code1.py
@@ -0,0 +1,6 @@
+# Finding the largest element in an array 
+# By using built in functions of python
+
+arr = [12,9,10,27,25,30,2]
+arr.sort()
+print("Largest element is : ", arr[-1])
---
+++ Day-1 (16/06/26)/code3.py
@@ -0,0 +1,14 @@
+# Reversing an array using two pointers technique
+
+def reverse_arr(arr):
+    l = 0               # Left pionter
+    r = len(arr)-1      # Right pointer
+    while l <= r:
+        arr[l], arr[r] = arr[r], arr[l]
+        l += 1
+        r -= 1
+
+    return arr 
+
+arr = [12, 9, 10, 27, 25, 30, 2]
+print(reverse_arr(arr))
