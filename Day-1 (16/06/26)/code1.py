069b817814a2dbc0132e887bb034ad79c670971f
Soham Srivastava <sohamsri17@gmail.com>
2026-06-16 14:37:24 +0530

    Refactor to find largest element using max function

--- Day-1 (16/06/26)/code1.py
+++ Day-1 (16/06/26)/code1.py
@@ -2,6 +2,6 @@
 # By using built in functions of python
 # Time Complexity : O(n)
 
-arr = [12,9,10,27,25,30,2]
-arr.sort()
-print("Largest element is : ", arr[-1])
+arr = [12, 9, 10, 27, 25, 30, 2]
+largest = max(arr) 
+print("Largest element is:", largest)
