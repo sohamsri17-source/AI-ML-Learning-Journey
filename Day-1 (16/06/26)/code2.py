8b5613819725d36370f88f77a64f16592aec2041
Soham Srivastava <sohamsri17@gmail.com>
2026-06-16 13:46:39 +0530

    Add code to find second largest element in array

---
+++ Day-1 (16/06/26)/code2.py
@@ -0,0 +1,7 @@
+# Finding the second largest element in an array 
+# By using built in functions of python
+# Time complexity : O(nlogn)
+
+arr = [12, 9, 10, 27, 25, 30, 2]
+arr.sort()
+print("Second largest is:", arr[-2])  
