4fba2310a032a61b9f61c3a604ab1531c5bec82a
Soham Srivastava <sohamsri17@gmail.com>
2026-06-17 19:59:03 +0530

    Update code3.py

--- Day-2 (17'06'26)/code3.py
+++ Day-2 (17'06'26)/code3.py
@@ -3,5 +3,5 @@ import numpy as  np
 array1 = np.array([1,2,3,4,5,6,7,8,9,10])
 array2 = np.array([11,12,13,14,15,16,17,18,19,20])
 
-print(array1+array2)   # Element-wise addition, output = 
-print(array1*array2)   # Element-wise multiplication
+print(array1+array2)   # Element-wise addition, output = [12 14 16 18 20 22 24 26 28 30]
+print(array1*array2)   # Element-wise multiplication, output = [ 11  24  39  56  75  96 119 144 171 200]
