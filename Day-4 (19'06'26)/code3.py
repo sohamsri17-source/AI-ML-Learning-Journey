5c20b9bf8f1074542b07cb29f4e846f52de40814
Soham Srivastava <sohamsri17@gmail.com>
2026-06-19 21:15:33 +0530

    Update code3.py

--- Day-4 (19'06'26)/code3.py
+++ Day-4 (19'06'26)/code3.py
@@ -22,4 +22,13 @@ print(f"Sum of each column (axis=0): {column_sums}")
 
 print(f"Mean of each row (axis=1): {row_means}")
 
-"""Output :  """
+""" Output:-
+
+Original Random Array:
+[44 45 45 10 21 23 36 16 43 11]
+
+Boolean Mask (array > 30):
+[ True  True  True False False False  True False  True False]
+
+Values greater than 30:
+[44 45 45 36 43] """
